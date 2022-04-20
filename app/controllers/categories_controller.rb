9dea913a5d8181e1c0cd15e17c31f49a7342d1ae
naufal <naufalazmi020@gmail.com>
2022-04-20 15:32:45 +0000

    passed get detail category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,12 @@ class CategoriesController < ApplicationController
 
     render json: response_with_data(:success, { categories: @categories.map { |category| category.as_response}}), status: :ok
   end
+
+  def show
+    @category = Category.find(params[:id])
+    render json: response_with_data(:success, { category: @category.as_response }), status: :ok
+    
+  rescue ActiveRecord::RecordNotFound
+    render json: response_with_message(:not_found, 'Kategori tidak ditemukan'), status: :not_found
+  end
 end
