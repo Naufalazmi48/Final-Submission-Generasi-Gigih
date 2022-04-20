cca100ca2b7fe3cd0a4c57cd6bd5879de2d720ad
naufal <naufalazmi020@gmail.com>
2022-04-20 15:30:12 +0000

    added get detail category

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -21,5 +21,33 @@ RSpec.describe 'Categories', type: :request do
     end
   end
 
-  
+  describe 'GET /show' do
+    it 'Should return exception when invalid category id' do
+      # Given
+      category = FactoryBot.create(:category)
+      expected = {
+        status: :not_found,
+        message: "Kategori tidak ditemukan"
+      }.to_json
+      # When
+      get '/categories/xyz'
+      # then
+      expect(response.body).to eq(expected.to_s)
+      expect(response).to have_http_status(404)
+    end
+
+    it 'should return correct category when using valid id' do
+       # Given
+      category = FactoryBot.create(:category)
+      expected = {
+        status: :success,
+        data: { category: category.as_response}
+      }.to_json
+      # When
+      get "/categories/#{category.id}"
+      # then
+      expect(response.body).to eq(expected.to_s)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
