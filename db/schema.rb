03ba24fe7857151773c4157ab2136119e3adaca8
naufal <naufalazmi020@gmail.com>
2022-04-21 06:49:26 +0000

    added table, controller, and passed testing order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_051000) do
+ActiveRecord::Schema.define(version: 2022_04_21_055824) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2022_04_21_051000) do
     t.string "name"
     t.float "price"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_id"
+    t.string "date"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
