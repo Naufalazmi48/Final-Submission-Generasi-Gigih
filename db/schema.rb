464bcfdd49849b1afbf61cd0691d4b21e178e599
naufal <naufalazmi020@gmail.com>
2022-04-21 05:12:27 +0000

    create table customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_013719) do
+ActiveRecord::Schema.define(version: 2022_04_21_051000) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "food_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
